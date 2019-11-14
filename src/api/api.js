1c7924c8abd6e3962729e894f8af282103c098f2
tesh254 <ewachira254@gmail.com>
2019-11-14 03:44:14 +0300

    FEATURE: fetch all articles saved in the database

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import Routes from './routes'
 import { loginAdmin, signupUser } from './middlewares/auth';
+import { fetchArticles } from './middlewares/article';
 
 const api = express.Router()
 
@@ -10,5 +11,10 @@ const api = express.Router()
 api.post(Routes.auth.login, loginAdmin)
 
 api.post(Routes.auth.signup, signupUser)
+
+/**
+ * Article endpoints
+ */
+api.get(Routes.articles["fetch-all"], fetchArticles)
 
 export default api;
--- src/api/models/auth.js
+++ src/api/models/auth.js
@@ -1,5 +1,5 @@
 import nedb from "nedb";
 
-const articleDb = new nedb({ filename: "users.db", autoload: true });
+const authDb = new nedb({ filename: "users.db", autoload: true });
 
-export default articleDb;
+export default authDb;
